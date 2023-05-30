3fbcf2faa25356cc8491d4549a79d2b7c6e8d06a
marvy896 <onyex896@gmail.com>
2023-05-30 09:54:57 +0100

    Update to the contact list

--- contactbook/contact.py
+++ contactbook/contact.py
@@ -1,23 +1,37 @@
-print("Welcome to Marv's Contact Book")
+import os
+print("Welcome to Marv's Contact Book \n")
 contactNames = []
-phoneNumbers = []
-numOfContacts = 2
-options = input('Add (a) Search (s) Quit (q)')
+
+
+options = input('Add (a) Search (s) Quit (q) Print (p) Delete (d): ')
 if options == 'a':
-    for i in range(numOfContacts):
-        with open("contactlist", "a") as f:
-            contactName = input("Name: ")
-            phoneNumber = input("Phone Number: ")
-            f.writelines((contactName, " : ", phoneNumber, "\n"))
-            contactNames.append(contactName)
-            phoneNumbers.append(phoneNumber)
-            print("\nName\t\t\tPhone Number\n")
-            print("{}\t\t\t{}".format(contactNames, phoneNumbers))
+    if os.path.exists("contactlist"):
+        with open("contactlist", "r") as f:
+            for i in f:
+                contactNames.append(i)
+    contactName = input("Name: ")
+    phoneNumber = input("Phone Number: ")
+    contactNames.append(contactName + " : " + phoneNumber + "\n")
+    contactNames.sort(key= str.lower)
+    with open("contactlist", "w") as f:
+        f.writelines(contactNames)
 elif options == 's':
     with open("contactlist", "r") as f:
         searchContact = input("\nEnter Search Term: ")
         for i in f:
             if searchContact in i:
                 print(i)
+elif options == 'p':
+    with open("contactlist", "r") as f:
+        for i in f:
+            print(i)
+elif options == 'd':
+    with open("contactlist", "r") as f:
+        deleteOption = input("Enter delete term: ")
+        for i in f:
+            if deleteOption.lower() not in i.lower():
+                contactNames.append(i)
+    with open("contactlist", "w") as f:
+        f.writelines(contactNames)
 else:
     print("Goodbye")
